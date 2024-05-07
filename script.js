0670048afda5d7d4c4b88b71fa42bf69cfb74fae
drevelob <140353802+drevelob@users.noreply.github.com>
2024-05-07 13:23:42 -0500

    Add cards source and initial variable in script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const CARD_SOURCE = [
+  {
+    name: 'pink',
+    img: './images/00_char_pink.jpg'
+  },
+  {
+    name: 'yellow',
+    img: './images/01_char_yellow.jpg'
+  },
+  {
+    name: 'orange',
+    img: './images/02_char_orange.jpg'
+  },
+  {
+    name: 'green',
+    img: './images/03_char_green.jpg'
+  },
+  {
+    name: 'red',
+    img: './images/04_char_red.jpg'
+  },
+  {
+    name: 'blue',
+    img: './images/05_char_blue.jpg'
+  },
+  {
+    name: 'purple',
+    img: './images/06_char_purple.jpg'
+  },
+  {
+    name: 'black',
+    img: './images/07_char_black.jpg'
+  },
+];
+const cardContainer = document.querySelector('#grid');
